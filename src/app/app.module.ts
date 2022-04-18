956a4a4138e889b6be5d9a6c28630647fba66002
Joy Marocho <joy.marocho@student.moringaschool.com>
2022-04-18 07:35:06 +0300

    creating components from source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateCountPipe } from './date-count.pipe';
 import { HighlightDirective } from './highlight.directive';
+import { ComponentsComponent } from './components/components.component';
+import { ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateCountPipe,
-    HighlightDirective
+    HighlightDirective,
+    ComponentsComponent,
+    ModelsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
