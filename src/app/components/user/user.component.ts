8e045092fd8e5ea50a5f09f6853ff8c12ea37317
JoyMarocho <joymarocho@gmail.com>
2022-04-20 00:59:25 +0300

    making user ts

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UsersearchService } from 'src/app/services/usersearch.service';
+import { Repo } from 'src/app/models/repo';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+  repos: any;
+
+  constructor(public userService: UsersearchService) { }
+
+  searchGithubUser(login:string){
+    this.userService.getUserProfile(login).then((success)=>{
+      this.user = this.userService.user;
+    },
+    (error)=>{
+      console.log(error)
+    });
+
+    this.userService.getUserRepo(login).then((success)=>{
+      this.repos = this.userService.repos;
+    },
+    (error)=>{
+      console.log(error)
+    });
+  }
+
 
   ngOnInit(): void {
+    this.searchGithubUser(JoyMarocho)
   }
 
 }
