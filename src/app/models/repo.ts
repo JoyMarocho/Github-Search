77a5f8e1838b8de7f51829ba683679201dfd9e09
JoyMarocho <joymarocho@gmail.com>
2022-04-20 01:00:32 +0300

    declaring a class

--- src/app/models/repo.ts
+++ src/app/models/repo.ts
@@ -1,2 +1,3 @@
 export class Repo {
+    constructor(public name: string,public html_url:string,public description:string,public language:string, public created_at:Date, public downloads_url:string){}
 }
