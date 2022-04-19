f945f23e0c5ff9ac21de83874fe9c2cd4e3f9976
JoyMarocho <joymarocho@gmail.com>
2022-04-20 01:17:16 +0300

    importing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchuserComponent } from './components/searchuser/searchuser.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'user', component: UserComponent},
+  {path:'searchuser', component: SearchuserComponent},
+  {path:'navbar', component: NavbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
