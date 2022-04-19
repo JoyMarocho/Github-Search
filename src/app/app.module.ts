3374beeb8d1b2e4c90b5f5caf5ff76e21007fe70
JoyMarocho <joymarocho@gmail.com>
2022-04-20 01:23:11 +0300

    importing modules on ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DateCountPipe } from './date-count.pipe';
-import { HighlightDirective } from './highlight.directive';
 import { ComponentsComponent } from './components/components.component';
 import { ModelsComponent } from './models/models.component';
 import { ServicesComponent } from './services/services.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchuserComponent } from './components/searchuser/searchuser.component';
 import { UserComponent } from './components/user/user.component';
+import { UsersearchService } from './services/usersearch.service';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateCountPipe,
-    HighlightDirective,
-    ComponentsComponent,
-    ModelsComponent,
-    ServicesComponent,
+    HighlightDirective,    
     NavbarComponent,
     SearchuserComponent,
     UserComponent
+    ComponentsComponent,
+    ModelsComponent,
+    ServicesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
